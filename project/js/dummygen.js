9b078f3d5a98fef8e971a54e5bbe20bde5a15c63
NickuWasTaken <74908215+NickuWasTaken@users.noreply.github.com>
2022-11-03 14:12:04 +0100

    Idk

---
+++ project/js/dummygen.js
@@ -0,0 +1,19 @@
+/*
+for (var i = 1; i <= 20; i++) {
+	let randomBit = Math.round(Math.random());
+	let randomBit2 = Math.round(Math.random());
+	let randomTopic = Math.ceil(Math.random()*21);
+	let randomUser = Math.ceil(Math.random()*2);
+	let randomGender = Math.ceil(Math.random()*3);
+	let randomDepartment = Math.ceil(Math.random()*6);
+	let randomEducation = Math.ceil(Math.random()*10);
+	let query = "INSERT INTO registration (userID, departmentID, educationID, conversation_time, registration_time, resident, is_student) VALUES ("+randomUser+", "+randomDepartment+", "+randomEducation+", '23:59:59.9999999', CURRENT_TIMESTAMP, "+randomBit+", "+randomBit2+"); <br>"
+	document.getElementById("id").innerHTML += query;
+}
+*/
+
+for (var i = 1; i <= 20; i++) {
+	let randomMedia = Math.ceil(Math.random()*3);
+	let query = "INSERT INTO registration_gender (registrationID, genderID) VALUES ("+i+", "+randomMedia+"); <br>"
+	document.getElementById("id").innerHTML += query;
+}
